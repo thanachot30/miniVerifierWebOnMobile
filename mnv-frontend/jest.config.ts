db67fb31a711d2af1cdc876062d65c8e2ce1fe13
Patsawut Tananchai <Patsawut.Tananchai@bangkokbank.com>
2025-01-10 11:41:29 +0700

    initialize project structure with shared library and frontend/backend applications

---
+++ mnv-frontend/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'mnv-frontend',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', tsx: true },
+          transform: { react: { runtime: 'automatic' } },
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/mnv-frontend',
+};
